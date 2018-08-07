c5e01c43f1b74b449ed6981cdba127d09b2ae2f3
miaofeng <miaofeng@ec.com.cn>
2018-08-07 09:44:33 +0800

    增加util类

--- commonutil/src/main/java/cn/com/gfa/android/commonutil/utils/ClipboardUtil.java
+++ commonutil/src/main/java/cn/com/gfa/android/commonutil/utils/ClipboardUtil.java
@@ -1,8 +1,10 @@
 package cn.com.gfa.android.commonutil.utils;
 
 import android.content.ClipData;
-import android.content.Context;
 import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 
 public class ClipboardUtil {
@@ -67,5 +69,77 @@ public class ClipboardUtil {
             sb.append(mClipboardManager.getText());
         }
         return sb.toString();
+    }
+
+    /**
+     * 复制文本到剪贴板
+     *
+     * @param text 文本
+     */
+    public static void copyText(final CharSequence text) {
+        ClipboardManager clipboard = (ClipboardManager)ApplicationUtil.getApp().getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboard.setPrimaryClip(ClipData.newPlainText("text", text));
+    }
+
+    /**
+     * 获取剪贴板的文本
+     *
+     * @return 剪贴板的文本
+     */
+    public static CharSequence getText() {
+        ClipboardManager clipboard = (ClipboardManager)ApplicationUtil.getApp().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = clipboard.getPrimaryClip();
+        if (clip != null && clip.getItemCount() > 0) {
+            return clip.getItemAt(0).coerceToText(ApplicationUtil.getApp());
+        }
+        return null;
+    }
+
+    /**
+     * 复制uri到剪贴板
+     *
+     * @param uri uri
+     */
+    public static void copyUri(final Uri uri) {
+        ClipboardManager clipboard = (ClipboardManager)ApplicationUtil.getApp().getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboard.setPrimaryClip(ClipData.newUri(ApplicationUtil.getApp().getContentResolver(), "uri", uri));
+    }
+
+    /**
+     * 获取剪贴板的uri
+     *
+     * @return 剪贴板的uri
+     */
+    public static Uri getUri() {
+        ClipboardManager clipboard = (ClipboardManager)ApplicationUtil.getApp().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = clipboard.getPrimaryClip();
+        if (clip != null && clip.getItemCount() > 0) {
+            return clip.getItemAt(0).getUri();
+        }
+        return null;
+    }
+
+    /**
+     * 复制意图到剪贴板
+     *
+     * @param intent 意图
+     */
+    public static void copyIntent(final Intent intent) {
+        ClipboardManager clipboard = (ClipboardManager)ApplicationUtil.getApp().getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboard.setPrimaryClip(ClipData.newIntent("intent", intent));
+    }
+
+    /**
+     * 获取剪贴板的意图
+     *
+     * @return 剪贴板的意图
+     */
+    public static Intent getIntent() {
+        ClipboardManager clipboard = (ClipboardManager)ApplicationUtil.getApp().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = clipboard.getPrimaryClip();
+        if (clip != null && clip.getItemCount() > 0) {
+            return clip.getItemAt(0).getIntent();
+        }
+        return null;
     }
 }
